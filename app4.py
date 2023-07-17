ddc47509db13f24984a9df082efc23bacf9b4237
Achini-Hewawitharana <achini@dxdy.tech>
2023-07-17 14:04:32 +0530

    all commint before tiktoken

--- app4.py
+++ app4.py
@@ -15,7 +15,9 @@ from langchain.chains.question_answering import load_qa_chain
 app = Flask(__name__)
 
 # Get the OpenAI API key from environment variable
-openai_api_key = os.environ.get("OPENAI_API_KEY")
+# openai_api_key = os.environ.get("OPENAI_API_KEY")
+
+openai_api_key = "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"
 
 # Set the OpenAI API key if it exists
 if openai_api_key:
@@ -56,7 +58,6 @@ def get_source_chunks(repo_path, pdf_folder_path):
 
 # Function to generate response from user input
 def generate_response(input_text):
-
     # Define the path of the repository and Chroma DB
     # to get the absolute path of the current script file
     REPO_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -78,45 +79,45 @@ def generate_response(input_text):
         vector_db = Chroma(persist_directory=CHROMA_DB_PATH, embedding_function=OpenAIEmbeddings())
 
     # Load a QA chain
-    qa_chain = load_qa_chain(OpenAI(temperature=1), chain_type="stuff")
+    qa_chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
     qa = RetrievalQA(combine_documents_chain=qa_chain, retriever=vector_db.as_retriever())
     query_response = qa.run(input_text)
     print("query_response =", query_response)
 
+    # # Generate a response from ChatGPT
+    # chat_response = openai.ChatCompletion.create(
+    #     model="gpt-3.5-turbo",
+    #     messages=[
+    #         {"role": "system", "content": "You are a helpful assistant."},
+    #         {"role": "user", "content": input_text}
+    #     ]
+    # )
+    # chat_response_text = chat_response.choices[0].message.content
+
+    # # Combine the response from the QA chain and ChatGPT
+    # combined_response = query_response + "\n" + chat_response_text
+    combined_response = query_response
+
     # Example response object
     response = {
         "role": "bot",  # Add the role to identify the sender
-        "content": query_response  # Store the response text
+        "content": combined_response  # Store the combined response text
     }
-
-    # Update the session_state with the response message
-    # session_state['messages'].append(response)
 
     return response
 
 # Initialise session state variable
+# session_state = {
+#     'messages': [
+#         {"role": "system", "content": "You are a helpful assistant."}
+#     ]
+# }
+
 session_state = {
     'messages': [
-        {"role": "system", "content": "You are a helpful assistant."}
+        {"role": "system", "content": "Welcome! How can I assist you today?"}
     ]
 }
-
-# @app.route('/', methods=['GET', 'POST'])
-# def home():
-#     if request.method == 'POST':
-#         user_input = request.form.get('user_input')
-#         if user_input:
-#             query_response = generate_response(user_input)
-#             session_state.setdefault('past', []).append({"role": "user", "content": user_input})
-#             session_state.setdefault('generated', []).append({"role": "bot", "content": query_response['content']})
-#     else:
-#         query_response = generate_response("Hello")
-#         session_state.setdefault('past', []).append({"role": "user", "content": ""})
-#         session_state.setdefault('generated', []).append({"role": "bot", "content": query_response['content']})
-
-#     session_state['messages'] = session_state.get('past', []) + session_state.get('generated', [])
-
-#     return render_template('index3_3.html', messages=session_state['messages'])
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -126,12 +127,31 @@ def home():
             query_response = generate_response(user_input)
             session_state['messages'].append({"role": "user", "content": user_input})
             session_state['messages'].append({"role": "bot", "content": query_response['content']})
-    else:
+    elif len(session_state['messages']) == 0:
+        # Only display the initial message if there are no existing messages
         query_response = generate_response("Hello")
         session_state['messages'].append({"role": "user", "content": ""})
         session_state['messages'].append({"role": "bot", "content": query_response['content']})
 
-    return render_template('index3_2.html', messages=session_state['messages'])
+    return render_template('index4_1.html', messages=session_state['messages'])
+
+
+# @app.route('/', methods=['GET', 'POST'])
+# def home():
+#     if request.method == 'POST':
+#         user_input = request.form.get('user_input')
+#         if user_input:
+#             query_response = generate_response(user_input)
+#             session_state['messages'].append({"role": "user", "content": user_input})
+#             session_state['messages'].append({"role": "bot", "content": query_response['content']})
+#     else:
+#         # Only display the initial message on the first GET request
+#         if len(session_state['messages']) == 1:
+#             query_response = generate_response("Hello")
+#             session_state['messages'].append({"role": "user", "content": ""})
+#             session_state['messages'].append({"role": "bot", "content": query_response['content']})
+
+#     return render_template('index4_1.html', messages=session_state['messages'])
 
 
 # Run the Flask app
